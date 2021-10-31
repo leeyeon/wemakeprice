2120b6e0835a4c25566498245c3f06b44870d951
lyh <bbqgoood@nate.com>
2021-10-31 21:34:15 +0900

    parameter 값 Validation 검증

--- wemakeprice/src/main/java/com/lyh/task/dto/ParserRequestDto.java
+++ wemakeprice/src/main/java/com/lyh/task/dto/ParserRequestDto.java
@@ -1,5 +1,9 @@
 package com.lyh.task.dto;
 
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.URL;
+
 import com.lyh.task.common.ParserType;
 
 import lombok.Getter;
@@ -11,8 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ParserRequestDto {
 
+	@URL(message = "URL을 형식에 맞게 입력해주세요.")
 	private String url;
+	
+	@NotBlank(message = "타입을 입력해주세요.")
 	private ParserType type;
+	
+	@NotBlank(message = "숫자를 입력해주세요.")
 	private int unitNum;
 	
 }
